ccb9421a160b6a102fd1e46c46b73be99fbde7da
liuxiaoyu-git <xiaoyliu@redhat.com>
2021-02-09 10:58:21 +0000

    Update 12-ready-for-install.sh

--- 12-ready-for-install.sh
+++ 12-ready-for-install.sh
@@ -1,25 +1,15 @@
-echo "STEP：Create rhcos-iso and ignition Download Files"
+echo "STEP: Enable download ignition file"
 
-chmod -R 705 ${RHCOS_ISO_PATH}/*
-cat << EOF > /etc/httpd/conf.d/rhcos.conf
-Alias /rhcos-iso "${RHCOS_ISO_PATH}"
-<Directory "${RHCOS_ISO_PATH}">
-  Options +Indexes +FollowSymLinks
-  Require all granted
-</Directory>
-<Location /rhcos >
-  SetHandler None
-</Location>
-EOF
-
-chmod -R 705 ${IGN_PATH}/*.ign
+chmod 604 ${IGN_PATH}/*.ign
+chmod 604 ${IGN_PATH}/set-*
+ 
 cat << EOF > /etc/httpd/conf.d/ignition.conf
-Alias /ignition "${IGN_PATH}/../"
+Alias /${OCP_CLUSTER_ID} "${IGN_PATH}/../"
 <Directory "${IGN_PATH}/../">
   Options +Indexes +FollowSymLinks
   Require all granted
 </Directory>
-<Location /ignition >
+<Location /${OCP_CLUSTER_ID}>
   SetHandler None
 </Location>
 EOF
@@ -27,8 +17,13 @@ EOF
 systemctl restart httpd
 
 echo =============================================================================================
-echo ========================== All Files Will Be Download When Booting ==========================
-verifyURL http://${YUM_DOMAIN}/ignition/${OCP_CLUSTER_ID}/bootstrap.ign
-verifyURL http://${YUM_DOMAIN}/ignition/${OCP_CLUSTER_ID}/master.ign
-verifyURL http://${YUM_DOMAIN}/ignition/${OCP_CLUSTER_ID}/worker.ign
-verifyURL ${RHCOS_METAL_URL}
+echo ===================== These Files Must Be Downloaded When CoreOS Booting ====================
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/bootstrap.ign
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/master.ign
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/worker.ign
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/set-bootstrap
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/set-master-0
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/set-master-1
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/set-master-2
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/set-worker-0
+verifyURL http://${YUM_DOMAIN}/${OCP_CLUSTER_ID}/ignition/set-worker-1
