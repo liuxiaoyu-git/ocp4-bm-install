e77f5a6853433dd9ddcf17faca29dad6ee258dae
liuxiaoyu-git <xiaoyliu@redhat.com>
2021-02-09 10:53:36 +0000

    Update 5-dns.sh

--- 5-dns.sh
+++ 5-dns.sh
@@ -1,7 +1,7 @@
-echo "STEP：Install BIND Service"
+echo "STEP: Install BIND Service"
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 nmcli connection modify ${NET_IF_NAME} ipv4.dns ${DNS_IP}
-nmcli connection modify ${NET_IF_NAME} ipv4.gateway ${GATEWAY}
+nmcli connection modify ${NET_IF_NAME} ipv4.gateway ${GATEWAY_IP}
 systemctl restart network
 
 yum -y install bind bind-utils
@@ -10,6 +10,12 @@ restoreFile /etc/named.rfc1912.zones
 restoreFile /etc/named.conf
 sed -i -e "s/listen-on port.*/listen-on port 53 { any; };/" /etc/named.conf
 sed -i -e "s/allow-query.*/allow-query { any; };/" /etc/named.conf
+sed -i '/recursion yes;/a \
+        forward first; \
+        forwarders { 114.114.114.114; 8.8.8.8; };' /etc/named.conf
+sed -i -e "s/dnssec-enable.*/dnssec-enable no;/" /etc/named.conf
+sed -i -e "s/dnssec-validation.*/dnssec-validation no;/" /etc/named.conf
+
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 cat >> /etc/named.rfc1912.zones << EOF
 
@@ -45,7 +51,6 @@ cat > /var/named/${DOMAIN}.zone << EOF
 
 @             IN NS                         dns.${DOMAIN}.
 
-bastion       IN A                          ${BASTION_IP}
 support       IN A                          ${SUPPORT_IP}
 dns           IN A                          ${DNS_IP}
 ntp           IN A                          ${NTP_IP}
@@ -67,8 +72,6 @@ cat > /var/named/${OCP_CLUSTER_ID}.${DOMAIN}.zone << EOF
 
 @             IN NS                         dns.${DOMAIN}.
 
-dns.${DOMAIN}           IN A                          ${DNS_IP}
-
 lb             IN A                          ${LB_IP}
 
 api            IN A                          ${LB_IP}
@@ -78,13 +81,19 @@ api-int        IN A                          ${LB_IP}
 bootstrap      IN A                          ${BOOTSTRAP_IP}
 
 master-0       IN A                          ${MASTER0_IP}
+master-1       IN A                          ${MASTER1_IP}
+master-2       IN A                          ${MASTER2_IP}
 
 etcd-0         IN A                          ${MASTER0_IP}
+etcd-1         IN A                          ${MASTER1_IP}
+etcd-2         IN A                          ${MASTER2_IP}
 
 worker-0       IN A                          ${WORKER0_IP}
 worker-1       IN A                          ${WORKER1_IP}
 
 _etcd-server-ssl._tcp.${OCP_CLUSTER_ID}.${DOMAIN}. 8640 IN SRV 0 10 2380 etcd-0.${OCP_CLUSTER_ID}.${DOMAIN}.
+_etcd-server-ssl._tcp.${OCP_CLUSTER_ID}.${DOMAIN}. 8640 IN SRV 0 10 2380 etcd-1.${OCP_CLUSTER_ID}.${DOMAIN}.
+_etcd-server-ssl._tcp.${OCP_CLUSTER_ID}.${DOMAIN}. 8640 IN SRV 0 10 2380 etcd-2.${OCP_CLUSTER_ID}.${DOMAIN}.
 
 EOF
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -99,24 +108,24 @@ cat > /var/named/${NET_SEGMENT_PI}.in-addr.arpa.zone << EOF
                                         
 @                              IN NS       dns.${DOMAIN}.
 
-13.1.168.192.in-addr.arpa.     IN PTR      bastion.${DOMAIN}.
+${SUPPORT_PI}.in-addr.arpa.    IN PTR      support.${DOMAIN}.
+${DNS_PI}.in-addr.arpa.    IN PTR      dns.${DOMAIN}.
+${NTP_PI}.in-addr.arpa.    IN PTR      ntp.${DOMAIN}.
+${YUM_PI}.in-addr.arpa.    IN PTR      yum.${DOMAIN}.
+${NFS_PI}.in-addr.arpa.    IN PTR      nfs.${DOMAIN}.
 
-12.1.168.192.in-addr.arpa.     IN PTR      support.${DOMAIN}.
-12.1.168.192.in-addr.arpa.     IN PTR      dns.${DOMAIN}.
-12.1.168.192.in-addr.arpa.     IN PTR      ntp.${DOMAIN}.
-12.1.168.192.in-addr.arpa.     IN PTR      yum.${DOMAIN}.
-12.1.168.192.in-addr.arpa.     IN PTR      registry.${DOMAIN}.
-12.1.168.192.in-addr.arpa.     IN PTR      nfs.${DOMAIN}.
-12.1.168.192.in-addr.arpa.     IN PTR      lb.${OCP_CLUSTER_ID}.${DOMAIN}.
-12.1.168.192.in-addr.arpa.     IN PTR      api.${OCP_CLUSTER_ID}.${DOMAIN}.
-12.1.168.192.in-addr.arpa.     IN PTR      api-int.${OCP_CLUSTER_ID}.${DOMAIN}.
+${LB_PI}.in-addr.arpa.    IN PTR      lb.${OCP_CLUSTER_ID}.${DOMAIN}.
+${LB_PI}.in-addr.arpa.    IN PTR      api.${OCP_CLUSTER_ID}.${DOMAIN}.
+${LB_PI}.in-addr.arpa.    IN PTR      api-int.${OCP_CLUSTER_ID}.${DOMAIN}.
 
-100.1.168.192.in-addr.arpa.    IN PTR      bootstrap.${OCP_CLUSTER_ID}.${DOMAIN}.
+${BOOTSTRAP_PI}.in-addr.arpa.    IN PTR      bootstrap.${OCP_CLUSTER_ID}.${DOMAIN}.
 
-101.1.168.192.in-addr.arpa.    IN PTR      master-0.${OCP_CLUSTER_ID}.${DOMAIN}.
+${MASTER0_PI}.in-addr.arpa.    IN PTR      master-0.${OCP_CLUSTER_ID}.${DOMAIN}.
+${MASTER1_PI}.in-addr.arpa.    IN PTR      master-1.${OCP_CLUSTER_ID}.${DOMAIN}.
+${MASTER2_PI}.in-addr.arpa.    IN PTR      master-2.${OCP_CLUSTER_ID}.${DOMAIN}.
 
-110.1.168.192.in-addr.arpa.    IN PTR      worker-0.${OCP_CLUSTER_ID}.${DOMAIN}.
-111.1.168.192.in-addr.arpa.    IN PTR      worker-1.${OCP_CLUSTER_ID}.${DOMAIN}.
+${WORKER0_PI}.in-addr.arpa.    IN PTR      worker-0.${OCP_CLUSTER_ID}.${DOMAIN}.
+${WORKER1_PI}.in-addr.arpa.    IN PTR      worker-1.${OCP_CLUSTER_ID}.${DOMAIN}.
 
 EOF
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -136,7 +145,6 @@ echo --------------------- Forward Check DNS---------------------
 checkDNSIP nfs.${DOMAIN} "$NFS_IP"
 checkDNSIP dns.${DOMAIN} "$DNS_IP"
 checkDNSIP yum.${DOMAIN} "$YUM_IP"
-checkDNSIP registry.${DOMAIN} "$REGISTRY_IP"
 checkDNSIP ntp.${DOMAIN} "$NTP_IP"
 checkDNSIP lb.${OCP_CLUSTER_ID}.${DOMAIN}  "$LB_IP"
 checkDNSIP api.${OCP_CLUSTER_ID}.${DOMAIN} "$LB_IP"
@@ -144,22 +152,21 @@ checkDNSIP api-int.${OCP_CLUSTER_ID}.${DOMAIN}  "$LB_IP"
 checkDNSIP *.apps.${OCP_CLUSTER_ID}.${DOMAIN} "$LB_IP"
 checkDNSIP bootstrap.${OCP_CLUSTER_ID}.${DOMAIN} "$BOOTSTRAP_IP"
 checkDNSIP master-0.${OCP_CLUSTER_ID}.${DOMAIN}  "$MASTER0_IP"
+checkDNSIP master-1.${OCP_CLUSTER_ID}.${DOMAIN}  "$MASTER1_IP"
+checkDNSIP master-2.${OCP_CLUSTER_ID}.${DOMAIN}  "$MASTER2_IP"
 checkDNSIP etcd-0.${OCP_CLUSTER_ID}.${DOMAIN} "$MASTER0_IP"
+checkDNSIP etcd-1.${OCP_CLUSTER_ID}.${DOMAIN} "$MASTER1_IP"
+checkDNSIP etcd-2.${OCP_CLUSTER_ID}.${DOMAIN} "$MASTER2_IP"
 checkDNSIP worker-0.${OCP_CLUSTER_ID}.${DOMAIN} "$WORKER0_IP"
 checkDNSIP worker-1.${OCP_CLUSTER_ID}.${DOMAIN} "$WORKER1_IP"
 echo --------------------- Reverse Check DNS---------------------
-echo $BASTION_IP related DNS is
-dig -x $BASTION_IP +short
-echo $SUPPORT_IP related DNS is
-dig -x $SUPPORT_IP +short
-echo $BOOTSTRAP_IP related DNS is
-dig -x $BOOTSTRAP_IP +short
-echo $MASTER0_IP related DNS is
-dig -x $MASTER0_IP +short
-echo $WORKER0_IP related DNS is
-dig -x $WORKER0_IP +short
-echo $WORKER1_IP related DNS is
-dig -x $WORKER1_IP +short
+echo -e "$SUPPORT_IP related DNS is \n$(dig -x $SUPPORT_IP +short)"
+echo -e "$BOOTSTRAP_IP related DNS is $(dig -x $BOOTSTRAP_IP +short)"
+echo -e "$MASTER0_IP related DNS is $(dig -x $MASTER0_IP +short)"
+echo -e "$MASTER1_IP related DNS is $(dig -x $MASTER1_IP +short)"
+echo -e "$MASTER2_IP related DNS is $(dig -x $MASTER2_IP +short)"
+echo -e "$WORKER0_IP related DNS is $(dig -x $WORKER0_IP +short)"
+echo -e "$WORKER1_IP related DNS is $(dig -x $WORKER1_IP +short)"
 
 echo =============================================================================================
 echo ==================================== Complete DNS Config ====================================
