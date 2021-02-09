698972ec14c55e1d5f7867c39ca50957c9a381cc
liuxiaoyu-git <xiaoyliu@redhat.com>
2021-02-09 11:05:52 +0000

    Create 10-ignition-file.sh

---
+++ 10-ignition-file.sh
@@ -0,0 +1,113 @@
+echo "STEP：Create ssh-key for accessing CoreOS"
+
+rm -rf ${SSH_KEY_PATH}
+mkdir -p ${SSH_KEY_PATH}
+ssh-keygen -N '' -f ${SSH_PRI_FILE}				    ## 生成访问节点使用的公钥和私钥文件
+export PULL_SECRET="$(cat ${REDHAT_PULL_SECRET})"
+export SSH_PUB_STR="$(cat ${SSH_KEY_PATH}/id_rsa.pub)"     ## 节点使用的公钥文件内容
+
+echo "STEP：Create CoreOS ignition files"
+rm -rf ${IGN_PATH}
+mkdir -p ${IGN_PATH}
+
+cat << EOF > ${IGN_PATH}/install-config.yaml
+apiVersion: v1
+baseDomain: ${DOMAIN}
+compute:
+- hyperthreading: Enabled
+  name: worker
+  replicas: ${REPLICA_WORKER}
+controlPlane:
+  hyperthreading: Enabled
+  name: master
+  replicas: ${REPLICA_MASTER}
+metadata:
+  name: ${OCP_CLUSTER_ID}
+networking:
+  clusterNetworks:
+  - cidr: 10.128.0.0/14
+    hostPrefix: 23
+  networkType: OpenShiftSDN
+  serviceNetwork:
+  - 172.30.0.0/16
+platform:
+  none: {}
+fips: false
+pullSecret: '${PULL_SECRET}'
+sshKey: '${SSH_PUB_STR}'
+EOF
+#
+cp ${IGN_PATH}/install-config.yaml{,.`date +%Y%m%d%H%M`.bak}
+tar -xzf ${OCP_PATH}/ocp-installer/openshift-install-linux-${OCP_VER}.tar.gz -C /usr/local/sbin/
+openshift-install create manifests --dir ${IGN_PATH}
+sed -i 's/mastersSchedulable: true/mastersSchedulable: false/g' ${IGN_PATH}/manifests/cluster-scheduler-02-config.yml
+
+#================
+export NTP_CONF=$(cat << EOF | base64 -w 0
+server ntp.${DOMAIN} iburst
+driftfile /var/lib/chrony/drift
+makestep 1.0 3
+rtcsync
+logdir /var/log/chrony
+EOF)
+
+cat << EOF > ${IGN_PATH}/openshift/99_masters-chrony-configuration.yaml
+apiVersion: machineconfiguration.openshift.io/v1
+kind: MachineConfig
+metadata:
+  labels:
+    machineconfiguration.openshift.io/role: master
+  name: masters-chrony-configuration
+spec:
+  config:
+    ignition:
+      config: {}
+      security:
+        tls: {}
+      timeouts: {}
+      version: 3.1.0
+    networkd: {}
+    passwd: {}
+    storage:
+      files:
+      - contents:
+          source: data:text/plain;charset=utf-8;base64,${NTP_CONF}
+        mode: 420
+        overwrite: true
+        path: /etc/chrony.conf
+  osImageURL: ""
+EOF
+
+cat << EOF > ${IGN_PATH}/openshift/99_workers-chrony-configuration.yaml
+apiVersion: machineconfiguration.openshift.io/v1
+kind: MachineConfig
+metadata:
+  labels:
+    machineconfiguration.openshift.io/role: worker
+  name: workers-chrony-configuration
+spec:
+  config:
+    ignition:
+      config: {}
+      security:
+        tls: {}
+      timeouts: {}
+      version: 3.1.0
+    networkd: {}
+    passwd: {}
+    storage:
+      files:
+      - contents:
+          source: data:text/plain;charset=utf-8;base64,${NTP_CONF}
+        mode: 420
+        overwrite: true
+        path: /etc/chrony.conf
+  osImageURL: ""
+EOF
+
+
+openshift-install create ignition-configs --dir ${IGN_PATH}/
+
+echo =============================================================================================
+echo =============================== All Generated Ignition Files ================================
+ls -al ${IGN_PATH}/*.ign 
