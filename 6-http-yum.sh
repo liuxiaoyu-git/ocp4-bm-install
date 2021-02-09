7b41b60eabcb3c20245f686010099f8bff9cfdff
liuxiaoyu-git <xiaoyliu@redhat.com>
2021-02-09 10:54:08 +0000

    Update 6-http-yum.sh

--- 6-http-yum.sh
+++ 6-http-yum.sh
@@ -1,9 +1,9 @@
-echo "STEP：Install Httpd Service and Config Formal Yum"
+echo "STEP: Install Httpd Service and Config Formal Yum"
 
 yum -y install httpd
 systemctl enable httpd --now
 restoreFile /etc/httpd/conf/httpd.conf
-sed -i  '/Listen 80/cListen 8080' /etc/httpd/conf/httpd.conf
+sed -i '/Listen 80/cListen 8080' /etc/httpd/conf/httpd.conf
 chmod -R 705 /data
 cat << EOF > /etc/httpd/conf.d/yum.conf
 Alias /repo "${YUM_PATH}"
@@ -31,9 +31,9 @@ baseurl=http://${YUM_DOMAIN}/repo/rhel-7-server-extras-rpms/
 enabled=1
 gpgcheck=0
 
-[rhel-7-server-ose-4.3-rpms] 
-name=rhel-7-server-ose-4.3-rpms
-baseurl=http://${YUM_DOMAIN}/repo/rhel-7-server-ose-4.3-rpms/
+[rhel-7-server-ose] 
+name=rhel-7-server-ose
+baseurl=http://${YUM_DOMAIN}/repo/rhel-7-server-ose-4.6-rpms/
 enabled=1
 gpgcheck=0 
 
@@ -46,4 +46,4 @@ echo ===========================================================================
 echo ================================= Complete Config Formal Yum ================================
 verifyURL http://${YUM_DOMAIN}/repo/rhel-7-server-rpms/
 verifyURL http://${YUM_DOMAIN}/repo/rhel-7-server-extras-rpms/
-verifyURL http://${YUM_DOMAIN}/repo/rhel-7-server-ose-4.3-rpms/
+verifyURL http://${YUM_DOMAIN}/repo/rhel-7-server-ose-4.6-rpms/
