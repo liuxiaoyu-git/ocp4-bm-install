f6eb302eb51eeae078c238b745000168b84aca32
liuxiaoyu-git <xiaoyliu@redhat.com>
2021-02-18 07:34:25 +0000

    Create 7-ntp.sh

---
+++ 7-ntp.sh
@@ -0,0 +1,19 @@
+echo "STEP: Config Time Service"
+
+timedatectl set-timezone Asia/Shanghai
+# yum -y install chrony
+# systemctl enable chronyd --now
+restoreFile /etc/chrony.conf
+sed -i -e "s/^server*/#&/g" \
+       -e "s/#local stratum 10/local stratum 10/g" \
+       -e "s/#allow 192.168.0.0\/16/allow all/g" \
+       /etc/chrony.conf
+cat >> /etc/chrony.conf << EOF
+server ntp.${DOMAIN} iburst
+EOF
+
+systemctl restart chronyd
+
+echo =============================================================================================
+echo ==================================== Complete NTP Config ====================================
+chronyc sources -v
